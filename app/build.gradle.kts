997f67f74672b46b4a8cd5df4b793b37ce316f05
Alfredo <elizaldiromero14@gmail.com>
2025-09-21 16:06:48 -0600

    Get info from firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //estatus bar dependecia
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
 }
--- app/src/main/java/com/example/fadebarber/ui/employee/pages/DashboardPage.kt
+++ app/src/main/java/com/example/fadebarber/ui/employee/pages/DashboardPage.kt
@@ -1,26 +1,14 @@
 package com.example.fadebarber.ui.employee.pages
 
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,7 +17,8 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -44,19 +33,18 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fadebarber.R
-import kotlinx.coroutines.delay
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.drawBehind
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.fadebarber.data.model.Appointment
-import com.example.fadebarber.ui.employee.viewmodel.DashboardViewModel
+import com.example.fadebarber.data.DashboardViewModel
+import com.example.fadebarber.data.model.AppointmentData
+import com.example.fadebarber.ui.employee.components.BlinkingDot
+import com.example.fadebarber.ui.employee.components.CardAppointment
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.launch
 
 
@@ -66,15 +54,25 @@ import kotlinx.coroutines.launch
 fun DashboardPage(
     modifier: Modifier = Modifier,
     viewModel: DashboardViewModel = viewModel()
-
 ) {
-    val date by viewModel.date.observeAsState()
-    val appointments by viewModel.appointments.observeAsState()
+
     val scrollState = rememberScrollState()
-    var selectedAppointmentId by remember { mutableStateOf<Int?>(null) }
+    var selectedAppointment by remember { mutableStateOf<AppointmentData?>(null) }
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     val scope = rememberCoroutineScope()
+    val appointments by viewModel.appointments.collectAsState()
+    val services by viewModel.services.collectAsState()
+    val users by viewModel.users.collectAsState()
+    val promotion by viewModel.promotion.collectAsState()
+    val systemUiController = rememberSystemUiController()
 
+    SideEffect {
+        // Opción 1: color sólido
+        systemUiController.setStatusBarColor(
+            color = Color(0xFF0A1F66), // azul oscuro del gradiente
+            darkIcons = false           // íconos blancos
+        )
+    }
 
     Column(
         modifier = modifier
@@ -171,13 +169,13 @@ fun DashboardPage(
                         //Spacer(modifier = Modifier.width(160.dp)) // separación entre texto y punto
                         BlinkingDot(size = 12.dp)
                     }
-                    date?.let {
+
                         Spacer(modifier = Modifier.height(12.dp))
-                        Text(it.service, fontSize = 20.sp, color = Color.White)
+                        Text("Corte De Cabello", fontSize = 20.sp, color = Color.White)
                         Spacer(modifier = Modifier.height(12.dp))
-                        Text(it.client, fontSize = 18.sp, color = Color.White)
+                        Text("Edson", fontSize = 18.sp, color = Color.White)
                         Spacer(modifier = Modifier.height(12.dp))
-                    }
+
 
                     Button(
                         modifier = Modifier
@@ -221,157 +219,42 @@ fun DashboardPage(
                     .padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                appointments?.forEach { appointment ->
-                    ElevatedCard(
-                        elevation = CardDefaults.cardElevation(defaultElevation = 12.dp),
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = {
-                            selectedAppointmentId = appointment.id // asigna el ID de la cita
+                appointments.forEach { appointment ->
+                    CardAppointment(
+                        appointment = appointment,
+                        services = services,
+                        users = users,
+                        promotions = promotion,
+                        onAppointmentClick = {
+                            selectedAppointment = it
                             scope.launch { sheetState.show() }
-
                         }
-                    ) {
-                        Column(modifier = Modifier.padding(18.dp)) {
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.SpaceBetween
-                            ) {
-                                Text(appointment.service, fontSize = 18.sp, fontWeight = FontWeight.Bold)
-                                Text(appointment.hour, fontSize = 18.sp, fontWeight = FontWeight.Bold)
-
-                            }
-                            Spacer(modifier = Modifier.height(2.dp))
-                            Text(appointment.client, fontSize = 12.sp, color = Color.Gray, fontWeight = FontWeight.Bold)
-                            Spacer(modifier = Modifier.height(2.dp))
-                            Text(appointment.date, fontSize = 12.sp, color = Color.Gray, fontWeight = FontWeight.Bold)
-
-                        }
-                    }
-
-
-                    Spacer(modifier = Modifier.height(8.dp))
+                    )
                 }
-                if (selectedAppointmentId != null) {
-                    val appointment = appointments?.find { it.id == selectedAppointmentId }
 
-                    ModalBottomSheet(
-                        sheetState = sheetState,
-                        onDismissRequest = {
-                            scope.launch {
-                                sheetState.hide()
-                                if (!sheetState.isVisible) selectedAppointmentId = null
-                            }
-                        }
-                    ) {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(400.dp)
-                                .padding(16.dp)
-                        ) {
-                            appointment?.let {
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(16.dp), // opcional, para que no quede pegado
-                                    verticalArrangement = Arrangement.Center,
-                                    horizontalAlignment = Alignment.CenterHorizontally
-                                ) {
-                                    Text("Detalle de la cita", fontWeight = FontWeight.Bold, fontSize = 20.sp)
-                                }
-                                Spacer(modifier = Modifier.height(8.dp))
-                                Text("Servicio: ${it.service}")
-                                Text("Cliente: ${it.client}")
-                                Text("Hora: ${it.hour}")
-                                Text("Fecha: ${it.date}")
-                            }
-
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Button(onClick = {
-                                scope.launch {
-                                    sheetState.hide()
-                                    if (!sheetState.isVisible) selectedAppointmentId = null
-                                }
-                            }) {
-                                Text("Cerrar")
-                            }
-                        }
-                    }
-                }
             }
 
         }
-    }
-}
 
-
-@Composable
-fun BlinkingDot(
-    size: Dp = 16.dp,
-    color: Color = Color.Green,
-    blinkDuration: Int = 500 // milisegundos
-) {
-    var visible by remember { mutableStateOf(true) }
-
-    // Cambia el estado cada blinkDuration
-    LaunchedEffect(Unit) {
-        while (true) {
-            delay(blinkDuration.toLong())
-            visible = !visible
+        if (selectedAppointment != null) {
+            ModalBottomSheet(
+                sheetState = sheetState,
+                onDismissRequest = {
+                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                        if (!sheetState.isVisible) selectedAppointment = null
+                    }
+                }
+            ) {
+                Text("Detalles de la cita seleccionada: ${selectedAppointment!!.id}")
+            }
         }
     }
-
-    Box(
-        modifier = Modifier
-            .size(size)
-            .background(
-                color = color.copy(alpha = if (visible) 1f else 0f),
-                shape = CircleShape
-            )
-    )
 }
 
 
-@Composable
-fun Modifier.shimmerLoading(
-    colors: List<Color> = listOf(
-        Color.LightGray.copy(alpha = 0.2f),
-        Color.LightGray.copy(alpha = 0.5f),
-        Color.LightGray.copy(alpha = 0.2f)
-    ),
-    durationMillis: Int = 5000,
-    angle: Float = 45f // ángulo diagonal
-): Modifier {
-    val transition = rememberInfiniteTransition(label = "")
-    val translateAnim by transition.animateFloat(
-        initialValue = -300f,
-        targetValue = 300f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = durationMillis, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        ),
-        label = ""
-    )
 
-    return this.drawBehind {
-        val width = size.width
-        val height = size.height
 
-        val rad = Math.toRadians(angle.toDouble())
-        val xOffset = translateAnim * kotlin.math.cos(rad).toFloat()
-        val yOffset = translateAnim * kotlin.math.sin(rad).toFloat()
 
-        drawRect(
-            brush = Brush.linearGradient(
-                colors = colors,
-                start = Offset(xOffset, yOffset),
-                end = Offset(xOffset + width, yOffset + height)
-            ),
-            size = size
-        )
-    }
-}
 
 
 
