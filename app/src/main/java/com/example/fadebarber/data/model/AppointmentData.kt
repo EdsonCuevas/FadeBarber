00e829b86847fe4a229ef28ba748ae263d9ce7ab
EdsonCuevas <efelix@ucol.mx>
2025-09-24 13:29:07 -0600

    fix appointmentdata

--- app/src/main/java/com/example/fadebarber/data/model/AppointmentData.kt
+++ app/src/main/java/com/example/fadebarber/data/model/AppointmentData.kt
@@ -2,12 +2,10 @@ package com.example.fadebarber.data.model
 data class AppointmentData(
     val id: String = "",
     val idClient: String = "",
-    val idEmployee: String? = "",
-    val idPromotion: Int = 0,
     val idEmployee: String = "",
     val idPromotion: Int = 0,
     val dateAppointment: String = "",
-    val serviceId: Int = 0,
+    val serviceId: Int? = 0,
     val statusAppointment: Int = 0,
     val clientName: String = "",
     val serviceName: String? = "",
