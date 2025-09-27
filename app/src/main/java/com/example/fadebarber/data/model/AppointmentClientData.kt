6101116f407de52af78332a0c2f7b7533a810483
Alfredo <elizaldiromero14@gmail.com>
2025-09-26 19:19:32 -0600

    Modify of model and appoimentCard

--- app/src/main/java/com/example/fadebarber/data/model/AppointmentClientData.kt
+++ app/src/main/java/com/example/fadebarber/data/model/AppointmentClientData.kt
@@ -1,12 +1,14 @@
 package com.example.fadebarber.data.model
 
 data class AppointmentClientData (
-    val id: String = "",
-    val idClient: String = "",
-    val idEmployee: String = "",
-    val serviceId: List<Int?>,
-    val idPromotion: List<Int?>,
-    val dateAppointment: String = "",
-    val timeAppointment: String = "",
-    val statusAppointment: Int = 0,
+    val id: String? = "",
+    val idClient: String? = "",
+    val idEmployee: String? = "",
+    val serviceId: List<Int?> = emptyList(),
+    val idPromotion: List<Int?> = emptyList(),
+    val dateAppointment: String? = "",
+    val timeAppointment: String? = "",
+    val methodPayment:String = "",
+    val totalPrice:Int = 0,
+    val statusAppointment: Int? = 0,
 )
