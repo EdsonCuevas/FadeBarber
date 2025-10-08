2b7c0ba996339c93d8a8bcb1fe749983dda4b486
EdsonCuevas <efelix@ucol.mx>
2025-10-07 18:44:59 -0600

    fix cartItems persistence

--- app/src/main/java/com/example/fadebarber/ui/client/ClientScreens.kt
+++ app/src/main/java/com/example/fadebarber/ui/client/ClientScreens.kt
@@ -1,7 +1,5 @@
 package com.example.fadebarber.ui.client
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
@@ -9,60 +7,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import com.example.fadebarber.data.AuthViewModel
 import com.example.fadebarber.data.HomeViewModel
 import com.example.fadebarber.ui.client.pages.CitaPageClient
 import com.example.fadebarber.ui.client.pages.CuentaPage
 import com.example.fadebarber.ui.client.pages.HomePage
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun ClientScreens(
     route: String,
     authViewModel: AuthViewModel,
-    homeViewModel: HomeViewModel = viewModel()
+    homeViewModel: HomeViewModel
 ) {
     val user = homeViewModel.currentUser.collectAsState().value
 
     when (route) {
         "home" -> {
             if (user != null) {
-                HomePage(user = user, viewModel = homeViewModel, authViewModel = authViewModel)
+                HomePage(user = user, viewModel = homeViewModel)
             } else {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
+                LoadingIndicator()
             }
         }
         "account" -> {
             if (user != null) {
                 CuentaPage(authViewModel = authViewModel)
             } else {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
+                LoadingIndicator()
             }
         }
-
         "date" -> {
             if (user != null) {
-                CitaPageClient(user =  user)
+                CitaPageClient(user = user)
             } else {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
+                LoadingIndicator()
             }
         }
+    }
+}
+
+@Composable
+private fun LoadingIndicator() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
     }
 }
