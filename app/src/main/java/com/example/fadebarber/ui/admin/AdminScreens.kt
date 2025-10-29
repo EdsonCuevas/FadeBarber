bb2a7e2426093798a5c91874d95580ac2e097671
EdsonCuevas <efelix@ucol.mx>
2025-10-28 18:05:52 -0600

    new admin template dashboard and new viewmodel

--- app/src/main/java/com/example/fadebarber/ui/admin/AdminScreens.kt
+++ app/src/main/java/com/example/fadebarber/ui/admin/AdminScreens.kt
@@ -2,13 +2,13 @@ package com.example.fadebarber.ui.admin
 
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
-import com.example.fadebarber.ui.admin.pages.EmployeeListScreen
+import com.example.fadebarber.ui.admin.pages.DashboardAdminScreen
 
 @Composable
 fun AdminScreens(route: String) {
     when (route) {
-        "dashboard" -> Text("Pantalla Dashboard de Admin")
-        "employee" -> EmployeeListScreen()
+        "dashboard" -> DashboardAdminScreen()
+        "employee" -> Text("Pantalla Empleados del Admin")
         "account" -> Text("Pantalla Cuenta Admin")
     }
 }
